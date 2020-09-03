7b6f759de6e8578472bf8fd4e8d4ce5371989343
alexLisouski <lisouski87@gmail.com>
2020-09-03 21:31:49 +0300

    benefits: finished benefits section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
-$(function(){
+// $(function(){
+// });
 
-
-
-  
-});
+// let div = document.createElement("div");
+// div.className = "msg";
+// div.innerHTML = "important!!!";
+// document.body.append(div);
