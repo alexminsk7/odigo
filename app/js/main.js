728da8ceada8847244de91d9f6cb36b9a79bc9b9
alexLisouski <lisouski87@gmail.com>
2020-10-22 14:22:03 +0300

    favicons: added favicons to project

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,7 @@
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.header__nav');
   const menuClose = document.querySelector('.header__close');
+  const headerLink = document.querySelectorAll('.header__link');
 
   burgerItem.addEventListener('click', () => {
     menu.classList.add('header__nav--active');
@@ -22,6 +23,17 @@
   menuClose.addEventListener('click', () => {
     menu.classList.remove('header__nav--active');
   });
+
+  if (window.innerWidth <= 767) {
+    headerLink.forEach((i) => {
+      i.addEventListener('click', () => {
+        menu.classList.remove('header__nav--active');
+      });
+    });
+    // for (let i = 0; i < headerLink.length; i++) {
+    //   console.log(headerLink[i]);
+    // }
+  }
 })();
 
 // Scroll to anchors
